a6a5b11dbb98c837a1b68d0a1968642125c87721
Dominik Lakatoš <deel77@gmail.com>
2018-08-08 14:53:14 +0200

    test

--- src/register/ArrayRegister.java
+++ src/register/ArrayRegister.java
@@ -20,7 +20,7 @@ public class ArrayRegister implements Register {
 	 */
 	public ArrayRegister(int size) {
 		persons = new Person[size];
-		count = 0; //nutne cislo 0
+		count = 0; //nutne cislo 0000
 	}
 
 	/* (non-Javadoc)
